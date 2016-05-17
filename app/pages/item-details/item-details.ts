100f9e894d266b5099203664762ff466a5451f73
Katherine Mello <katherine.mello8@gmail.com>
2016-05-17 12:19:17 -0400

    item details

--- app/pages/item-details/item-details.ts
+++ app/pages/item-details/item-details.ts
@@ -11,4 +11,7 @@ export class ItemDetailsPage {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
   }
+  navigateBackToPlanets = function(){
+  	this.nav.pop();
+  }
 }
